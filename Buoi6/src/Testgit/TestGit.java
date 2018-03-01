6b7b1ba65942bfaac82a5a7f3710078c9236d59e
du <du@TSWO>
2018-03-01 14:15:03 +0700

    abcxyz

--- Buoi6/src/Testgit/TestGit.java
+++ Buoi6/src/Testgit/TestGit.java
@@ -3,6 +3,9 @@ package Testgit;
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("Heloo ");
+		System.out.println("Comit git");
+		System.out.println("abc");
+		System.out.println("Hom nay la thu 5");
 	}
 
 }
